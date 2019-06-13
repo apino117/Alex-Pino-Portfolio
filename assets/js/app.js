02332096098207677b7648d5caba367ac6e147d1
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-06-13 12:14:11 -0400

    set up js for modals ahead of time

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// $("#user-input-modal").modal("show");
+// Code for the modals and thier respective on clicks
 
 $("#ex-dread-link").on("click", function() {
     $("#ex-dread-modal").modal("show");
@@ -10,4 +10,28 @@ $("#crystal-link").on("click", function() {
 
 $("#trivia-link").on("click", function() {
     $("#trivia-modal").modal("show");
+});
+
+$("#simpons-link").on("click", function() {
+    $("#simpsons-modal").modal("show");
+});
+
+$("#train-link").on("click", function() {
+    $("#train-modal").modal("show");
+});
+
+$("#find-philly-link").on("click", function() {
+    $("#find-philly-modal").modal("show");
+});
+
+$("#human-link").on("click", function() {
+    $("#human-modal").modal("show");
+});
+
+$("#burger-link").on("click", function() {
+    $("#burger-modal").modal("show");
+});
+
+$("#gritt-o-link").on("click", function() {
+    $("#gritt-o-modal").modal("show");
 });
