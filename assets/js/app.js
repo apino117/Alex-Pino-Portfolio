ce524a053b7313d4a24da60ac4214cfb6506b97c
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-06-13 12:11:02 -0400

    trivia game modal done

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,7 @@ $("#ex-dread-link").on("click", function() {
 $("#crystal-link").on("click", function() {
     $("#crystal-modal").modal("show");
 });
+
+$("#trivia-link").on("click", function() {
+    $("#trivia-modal").modal("show");
+});
